d33fa5ce57b64c401f7c8ea7de1cba01311e7d6f
Thibault Libert <thiblib@hotmail.com>
2019-12-18 10:57:35 +0100

    Better formatting and colouring depending on battery %

--- percentage/percentage/TrayIcon.cs
+++ percentage/percentage/TrayIcon.cs
@@ -3,21 +3,18 @@ using System.Drawing;
 using System.Runtime.InteropServices;
 using System.Windows.Forms;
 
-namespace percentage
-{
-    class TrayIcon
-    {
+namespace percentage {
+    class TrayIcon {
         [DllImport("user32.dll", CharSet = CharSet.Auto)]
         static extern bool DestroyIcon(IntPtr handle);
 
-        private const string iconFont = "Segoe UI";
-        private const int iconFontSize = 14;
+        private const string iconFont = "Consolas";
+        private const int iconFontSize = 12;
 
         private string batteryPercentage;
         private NotifyIcon notifyIcon;
 
-        public TrayIcon()
-        {
+        public TrayIcon() {
             ContextMenu contextMenu = new ContextMenu();
             MenuItem menuItem = new MenuItem();
 
@@ -43,49 +40,54 @@ namespace percentage
             timer.Start();
         }
 
-        private void timer_Tick(object sender, EventArgs e)
-        {
+        private void timer_Tick(object sender, EventArgs e) {
             PowerStatus powerStatus = SystemInformation.PowerStatus;
-            batteryPercentage = (powerStatus.BatteryLifePercent * 100).ToString();
+            float battery = (powerStatus.BatteryLifePercent * 100);
 
-            using (Bitmap bitmap = new Bitmap(DrawText(batteryPercentage, new Font(iconFont, iconFontSize), Color.White, Color.Black)))
-            {
+            Color bgColor = Color.White;
+            Color textColor = Color.Black;
+
+            if (battery >= 50) {
+                bgColor = Color.LightGreen;
+            } else if (battery >= 30) {
+                bgColor = Color.Yellow;
+            } else {
+                bgColor = Color.DarkRed;
+                textColor = Color.White;
+            }
+
+            batteryPercentage = String.Format("{0:00}", battery);
+
+            using (Bitmap bitmap = new Bitmap(DrawText(batteryPercentage, new Font(iconFont, iconFontSize), textColor, bgColor))) {
                 System.IntPtr intPtr = bitmap.GetHicon();
-                try
-                {
-                    using (Icon icon = Icon.FromHandle(intPtr))
-                    {
+                try {
+                    using (Icon icon = Icon.FromHandle(intPtr)) {
                         notifyIcon.Icon = icon;
                         notifyIcon.Text = batteryPercentage + "%";
                     }
-                }
-                finally
-                {
+                } finally {
                     DestroyIcon(intPtr);
                 }
             }
         }
 
-        private void menuItem_Click(object sender, EventArgs e)
-        {
+        private void menuItem_Click(object sender, EventArgs e) {
             notifyIcon.Visible = false;
             notifyIcon.Dispose();
             Application.Exit();
         }
 
-        private Image DrawText(String text, Font font, Color textColor, Color backColor)
-        {
+        private Image DrawText(String text, Font font, Color textColor, Color backColor) {
             var textSize = GetImageSize(text, font);
-            Image image = new Bitmap((int) textSize.Width, (int) textSize.Height);
-            using (Graphics graphics = Graphics.FromImage(image))
-            {
+            var textSize2Char = GetImageSize("22", font);
+            Image image = new Bitmap((int)textSize.Width, (int)textSize2Char.Height);
+            using (Graphics graphics = Graphics.FromImage(image)) {
                 // paint the background
                 graphics.Clear(backColor);
 
                 // create a brush for the text
-                using (Brush textBrush = new SolidBrush(textColor))
-                {
-                    graphics.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAlias;
+                using (Brush textBrush = new SolidBrush(textColor)) {
+                    graphics.TextRenderingHint = System.Drawing.Text.TextRenderingHint.ClearTypeGridFit;
                     graphics.DrawString(text, font, textBrush, 0, 0);
                     graphics.Save();
                 }
@@ -94,8 +96,7 @@ namespace percentage
             return image;
         }
 
-        private static SizeF GetImageSize(string text, Font font)
-        {
+        private static SizeF GetImageSize(string text, Font font) {
             using (Image image = new Bitmap(1, 1))
             using (Graphics graphics = Graphics.FromImage(image))
                 return graphics.MeasureString(text, font);
